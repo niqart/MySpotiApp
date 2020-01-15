e9b018e19418939edf186a72a81aa1b85ec81acd
Nico Araya <nlaraya@uc.cl>
2020-01-15 13:18:54 -0300

    spotify service and  get new releases

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  nuevasCanciones: any[] = [];
 
-  ngOnInit() {
+  constructor(private spotify: SpotifyService) { 
+    this.spotify.getNewReleases()
+      .subscribe((data:any) =>{
+        this.nuevasCanciones = data.albums.items;
+      });
   }
 
 }
---
+++ src/assets/receta.md
@@ -0,0 +1,10 @@
+## Apuntes relevantes
+
+#### Tags propios
+* En *< router-outlet >*, se carga el contenido dinamico desde RouterLink, como una caja que carga esa vista parcial.
+
+#### Services
+* Este decorador provee el servicio para la app, no es necesario importarlo en app.module  
+@Injectable({
+  providedIn: 'root'
+}) 
